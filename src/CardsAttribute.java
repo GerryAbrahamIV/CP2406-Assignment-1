9932a31172bd8ed8b94aba71068d9e3896a335f1
Lucasthefourth <gerry97abraham@yahoo.com>
2016-04-04 15:39:06 +0800

    Can now select specific cards and attributes

--- src/CardsAttribute.java
+++ src/CardsAttribute.java
@@ -1,4 +1,44 @@
+
 public class CardsAttribute {
+
+// Name = 0,Hardness = 1,Gravity = 2,Cleavage = 3,Crustal Abundance = 4,EcoValue = 5:
+    public String printCardDetails(String line, int attCode){
+
+        String currentCardStat[] = line.split(",");
+        return currentCardStat[attCode];
+}
+
+    public String getName(String line){
+        String currentCardStat[] = line.split(",");
+        return currentCardStat[0];
+    }
+    public String getHardness(String line){
+
+        String currentCardStat[] = line.split(",");
+        return currentCardStat[1];
+    }
+    public String getGravity(String line){
+
+        String currentCardStat[] = line.split(",");
+        return currentCardStat[2];
+    }
+    public String getCleavage(String line){
+
+        String currentCardStat[] = line.split(",");
+        return currentCardStat[3];
+    }
+    public String getAbundance(String line){
+
+        String currentCardStat[] = line.split(",");
+        return currentCardStat[4];
+    }
+    public String getEcoValue(String line){
+
+        String currentCardStat[] = line.split(",");
+        return currentCardStat[5];
+    }
+
+
 
 
 }
--- src/Javassignment.java
+++ src/Javassignment.java
@@ -1,31 +1,75 @@
+import javax.swing.*;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
+public class Javassignment {
+    public static void main(String[] args) {
 
-public class Javassignment
+        List<String> cardList = new ArrayList<>();
 
-{
-    public static void main(String[] args) throws IOException {
-                //Start of file reading
+        //Start of file reading
         String file_name = "card.txt";
         try {
             ReadFile file = new ReadFile(file_name);
             String[] cardLines = file.OpenFile();
 
-            int i, x;
+            int i;
 
-            for ( i=0; i < cardLines.length; i++ )
-            {   String currentCardStat[] = cardLines[i].split(",");
+            for (i = 0; i < cardLines.length; i++) //loop the lines
+            {
+                String currentCardStat[] = cardLines[i].split(",");     //
                 // Name = 0,Hardness = 1,Gravity = 2,Cleavage = 3,Crustal Abundance = 4,EcoValue = 5
-                System.out.println("Name: " + currentCardStat[0] + ", Hardness: " + currentCardStat[1] + ", Gravity: " + currentCardStat[2]); //testing
+                System.out.println("Name: " + currentCardStat[0] + ", Hardness: " + currentCardStat[1] + ", Gravity: " + currentCardStat[2]+ ", Cleavage: " + currentCardStat[3]); //testing
 
-
+                cardList.add(cardLines[i]);
             }
-        }
-        catch (IOException e){
+        } catch (IOException e) {
             System.out.println(e.getMessage());
         }
         //end of file reading
 
+        int cardCode;
+
+        CardsAttribute cardDeck = new CardsAttribute();
+
+        String cardCodeString = JOptionPane.showInputDialog("Card code:");
+
+
+        cardCode = Integer.parseInt(cardCodeString);
+
+        boolean repeatValue;
+        String cardDetails;
+        repeatValue = true;
+        cardDetails = "";
+
+        while (repeatValue) {
+            repeatValue = false;
+            String cardAttString = JOptionPane.showInputDialog("Card Attribute, Name = 0,Hardness = 1,Gravity = 2,Cleavage = 3,Crustal Abundance = 4,EcoValue = 5: ");
+            int cardAtt = Integer.parseInt(cardAttString);
+            switch (cardAtt) {
+                case 0:
+                    cardDetails = cardDeck.getName(cardList.get(cardCode)); break;
+                case 1:
+                    cardDetails = cardDeck.getHardness(cardList.get(cardCode)); break;
+                case 2:
+                    cardDetails = cardDeck.getGravity(cardList.get(cardCode)); break;
+                case 3:
+                    cardDetails = cardDeck.getCleavage(cardList.get(cardCode)); break;
+                case 4:
+                    cardDetails = cardDeck.getAbundance(cardList.get(cardCode)); break;
+                case 5:
+                    cardDetails = cardDeck.getEcoValue(cardList.get(cardCode)); break;
+                default:
+                    repeatValue = true;
+                    JOptionPane.showMessageDialog(null, "Invalid input!");
+                    break;
+
+            }
+        }
+
+
+        JOptionPane.showMessageDialog(null, cardDetails);
 
 
 
